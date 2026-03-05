73267a5fb5ec33a5933674b5cb11aa61840fc01d
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-03-05 17:50:27 +0700

    menyelesaikan prosedur input

--- buubleSort_1.cpp
+++ buubleSort_1.cpp
@@ -4,7 +4,7 @@
 #include <string>
 using namespace std;
 
-int array[20]; //Deklarasi variable global array a dengan ukuran 20 (berapapun)
+int arr[20]; //Deklarasi variable global array a dengan ukuran 20 (berapapun)
 int n;         //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
 void input()
@@ -12,10 +12,25 @@ void input()
     int d;
     while (true)
     {
-        cout << "Masukkan banyaknya elemen pada array : "; // Output ke layar
-        cin >> n;                                         // Input dari pengguna
-        if (n <= 20)                                       // if n kurang dari atau sama dengan 20
-            break;                                         // Keluar dari Loop
+        cout << "Masukkan banyaknya elemen pada array : ";      // Output ke layar
+        cin >> n;                                               // Input dari pengguna
+        if (n <= 20)                                            // if n kurang dari atau sama dengan 20
+            break;                                              // Keluar dari Loop
         else
+        {                                                               // Jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";   // Output ke layar
+        }
     }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {                                           // Looping dengan i dimulai dari 0 hingga 20
+        cout << "Data ke-" << (i + 1) << "; ";  // Output ke layar
+        cin >> arr[i];                          // Input dari pengguna
+    }
+
+
 }
