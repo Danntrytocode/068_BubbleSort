2ca60feee8b2f07ea4d7faf1204985cc98b5587f
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-03-05 18:47:12 +0700

    menambahkan prosedur penampilan elemen array dan program utama

--- buubleSort_1.cpp
+++ buubleSort_1.cpp
@@ -61,3 +61,36 @@ void bubbleSortArray()
     } while (pass <= n - 1);    // step 5
 }
 
+void display()  //Prosedur menampilkan elemen array yang sudah tersusun
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j]; // menampilkan array
+        if (j < n - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl;  // menampilkan jumlah dari pass
+    cout << endl;
+    cout << endl;
+}
+
+int main()
+{
+    input();
+
+    bubbleSortArray();
+    display();
+
+    system("pause");
+    return 0;
+}
