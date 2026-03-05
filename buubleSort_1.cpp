9cec2186d6f028489592be938017e19056269684
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-03-05 17:43:33 +0700

    menambahkan prosedur untuk input

--- buubleSort_1.cpp
+++ buubleSort_1.cpp
@@ -6,3 +6,16 @@ using namespace std;
 
 int array[20]; //Deklarasi variable global array a dengan ukuran 20 (berapapun)
 int n;         //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+void input()
+{ // Prosedur untuk input
+    int d;
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array : "; // Output ke layar
+        cin >> n;                                         // Input dari pengguna
+        if (n <= 20)                                       // if n kurang dari atau sama dengan 20
+            break;                                         // Keluar dari Loop
+        else
+    }
+}
