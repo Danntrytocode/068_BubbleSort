d0056c00870ae59598efe94fd2614bfd14eda379
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-03-05 17:38:38 +0700

    menambahkan deklarasi variable

--- buubleSort_1.cpp
+++ buubleSort_1.cpp
@@ -2,3 +2,7 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
+
+int array[20]; //Deklarasi variable global array a dengan ukuran 20 (berapapun)
+int n;         //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
